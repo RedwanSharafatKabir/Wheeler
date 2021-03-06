bc6c75e2aa65a62c97fbd25288a279d7da7fe837
Redwan Sharafat Kabir <mohammadmunna478@gmail.com>
2021-03-06 22:59:10 +0600

    E-commerce app to buy your favourite car

--- app/src/main/java/com/example/wheeler/AppActions/StartActivity.java
+++ app/src/main/java/com/example/wheeler/AppActions/StartActivity.java
@@ -28,7 +28,7 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start);
 
         initImageBitmap();
